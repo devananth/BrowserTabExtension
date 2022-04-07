c42772478fa6aea7991876c7ecd93b67a0460223
Devananth <devananthnkl@gmail.com>
2022-04-07 18:14:53 +0530

    Feat : added two pages

---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useData } from "../context";
+import { DATA_ACTIONS } from "../utils";
+import { Greetings, Time } from "../components";
+
+const UserPage = () => {
+  const [mainFocus, setMainFocus] = useState("");
+
+  const { dataState, dataDispatch } = useData();
+
+  console.log(dataState);
+
+  const focusHandler = (event) => {
+    const focus = event.target.value.trim();
+    setMainFocus(focus);
+  };
+
+  const formHandler = (event) => {
+    event.preventDefault();
+    localStorage.setItem("mainFocus", mainFocus);
+    dataDispatch({
+      type: DATA_ACTIONS.SET_MAIN_FOCUS,
+      payload: mainFocus,
+    });
+  };
+
+  return (
+    <section className="d-flex col xy-center">
+      <Time />
+      <Greetings />
+      {dataState.mainFocus === null ? (
+        <form onSubmit={formHandler} className="txt-center m-1">
+          <label className="txt-center h3">
+            What's your main focus for today ?
+          </label>
+          <input
+            type="text"
+            onChange={focusHandler}
+            className="w-50 m-2 p-sm txt-5xl txt-center"
+            value={mainFocus}
+            placeholder={"Enter your main focus"}
+          />
+        </form>
+      ) : (
+        <h4>My Main Focus is : {dataState.mainFocus} </h4>
+      )}
+    </section>
+  );
+};
+
+export { UserPage };
