80883f89108288b323646e0bb78084eaf1df71b9
Devananth <devananthnkl@gmail.com>
2022-04-07 18:14:36 +0530

    add : created context for storing data

---
+++ src/context/data-context.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { initialDataState } from "../utils";
+import { dataReducer } from "../reducers";
+import { DATA_ACTIONS } from "../utils";
+
+const DataContext = createContext({});
+
+const DataProvider = ({ children }) => {
+  const [dataState, dataDispatch] = useReducer(dataReducer, initialDataState);
+
+  useEffect(() => {
+    const [userName, mainFocus] = [
+      localStorage.getItem("userName"),
+      localStorage.getItem("mainFocus"),
+    ];
+
+    const date = new Date();
+
+    dataDispatch({
+      type: DATA_ACTIONS.SET_INITIAL_DATA,
+      payload: { userName, mainFocus },
+    });
+
+    dataDispatch({
+      type: DATA_ACTIONS.SET_TIME,
+      payload: {
+        date: date,
+        hours: date.getHours(),
+        mins: date.getMinutes(),
+        secs: date.getSeconds(),
+      },
+    });
+  }, []);
+
+  const value = { dataState, dataDispatch };
+
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
+
+const useData = () => useContext(DataContext);
+
+export { useData, DataProvider };
